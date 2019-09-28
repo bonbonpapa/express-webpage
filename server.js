0546042e25217dc0e2a7769be73fa9f086be2dfc
bonbonpapa <dui.zhang@gmail.com>
2019-09-28 11:04:25 -0400

    update index.html

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get('/reset',(reg , res) => {
     count = 0
     res.send("<h1>The visited count has been reset to " + count + " time</h1>")
 })
+app.get('/count2',(reg , res) => {
+    console.log("I received a request to /count", count)
+    count++
+    res.send("<h1>Multiply the counter by 2 equals to " + 2 * count + "</h1>")
+})
 app.use('/static',express.static(__dirname + '/public'))
 app.listen(4000, () => {
     console.log("server started")
